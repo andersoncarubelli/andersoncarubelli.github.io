3484ee0e1160ba4c34eed69969b30d59c539bd5d
Anderson Carubelli <1820277+andersoncarubelli@users.noreply.github.comacarubelli@gmail.com>
2024-09-30 12:19:17 -0300

    new website version
    Signed-off-by: Anderson Carubelli <1820277+andersoncarubelli@users.noreply.github.comacarubelli@gmail.com>

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Anderson Carubelli";
+export const SITE_DESCRIPTION =
+  "Tech Innovator with 20+ Years of Building Solutions | From Code to Leadership | AI Enthusiast | Empowering Teams to Drive Impact";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
